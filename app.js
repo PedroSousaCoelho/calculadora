5e8d7facff99dd898b45fd0994d6aabce56b2ae3
PedroSousaCoelho <pedrosousacoelhoo@gmail.com>
2022-10-08 20:18:24 -0300

    1.3

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-
 function insert(num){
     var numero = document.getElementById('p-teste').innerHTML
-    document.getElementById('p-teste').innerHTML = numero + num
+    var numero1 = document.getElementById('p-teste').innerHTML = numero + num
+}
+function clean(){
+    document.getElementById('p-teste').innerHTML = ""
+}
+function back(){ 
+    var resultado = document.getElementById('p-teste').innerHTML
+    document.getElementById('p-teste').innerHTML = resultado.substring(0, resultado.length -1)
+}
+function calcular(){
+    var resultado = document.getElementById('p-teste').innerHTML
+
+    if(resultado){
+        document.getElementById('p-teste').innerHTML = eval(resultado)
+    }
 }
